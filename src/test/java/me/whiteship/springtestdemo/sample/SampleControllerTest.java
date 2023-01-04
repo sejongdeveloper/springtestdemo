839869cbdd32297bbad598cf2695c2744f736fd3
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-04 23:34:26 +0900

    테스트

--- src/test/java/me/whiteship/springtestdemo/sample/SampleControllerTest.java
+++ src/test/java/me/whiteship/springtestdemo/sample/SampleControllerTest.java
@@ -1,6 +1,7 @@
 package me.whiteship.springtestdemo.sample;
 
 import junit.framework.TestCase;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,10 +10,12 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.rule.OutputCapture;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -20,6 +23,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @WebMvcTest(SampleController.class)
 public class SampleControllerTest extends TestCase {
+    @Rule
+    public OutputCapture outputCapture = new OutputCapture();
+
     @MockBean
     SampleService mockSampleService;
 
@@ -32,5 +38,9 @@ public class SampleControllerTest extends TestCase {
 
         mockMvc.perform(get("/hello"))
                 .andExpect(content().string("hello whiteship"));
+
+        assertThat(outputCapture.toString())
+                .contains("holoman")
+                .contains("skip");
     }
 }
