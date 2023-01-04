3ec6cb3c7f846b1a6b9fc472ac2fd208e67f30f8
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-04 23:17:33 +0900

    테스트

--- src/test/java/me/whiteship/springtestdemo/sample/SampleControllerTest.java
+++ src/test/java/me/whiteship/springtestdemo/sample/SampleControllerTest.java
@@ -4,28 +4,28 @@ import junit.framework.TestCase;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
-@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SampleControllerTest extends TestCase {
     @Autowired
-    MockMvc mockMvc;
+    TestRestTemplate testRestTemplate;
+
+    @MockBean
+    SampleService mockSampleService;
 
     @Test
     public void hello() throws Exception {
-        mockMvc.perform(get("/hello"))
-                .andExpect(status().isOk())
-                .andExpect(content().string("hello keesun"))
-                .andDo(print());
+        when(mockSampleService.getName()).thenReturn("whiteship");
+
+        String result = testRestTemplate.getForObject("/hello", String.class);
+        assertThat(result).isEqualTo("hello whiteship");
     }
 }
