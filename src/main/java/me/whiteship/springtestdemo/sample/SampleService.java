bd7cc91a10f6f8cb362dcbed4888a15cb275d965
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-04 23:12:51 +0900

    테스트

---
+++ src/main/java/me/whiteship/springtestdemo/sample/SampleService.java
@@ -0,0 +1,11 @@
+package me.whiteship.springtestdemo.sample;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SampleService {
+
+    public String getName() {
+        return "keesun";
+    }
+}
